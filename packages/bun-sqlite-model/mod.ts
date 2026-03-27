6408d2b442581f76758075f11dbdb65fe2d7493f
2byte <oopsfix21@gmail.com>
2026-03-27 18:57:57 +0300

    trash exports

--- packages/bun-sqlite-model/mod.ts
+++ packages/bun-sqlite-model/mod.ts
@@ -3,8 +3,3 @@ export { Migration } from './src/Migration.ts';
 export type { MigrationFile, MigrationRecord } from './src/Migration.ts';
 export { ArtisanMigrationPlugin } from './src/ArtisanMigrationPlugin.ts';
 export { ArtisanMakeModelPlugin } from './src/ArtisanMakeModelPlugin.ts';
-export {
-	bootstrapArtisanProject,
-	type BootstrapArtisanProjectOptions,
-	type BootstrapArtisanProjectResult,
-} from './src/bootstrapArtisanProject.ts';
